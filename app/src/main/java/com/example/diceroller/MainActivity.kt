f7965e9655577e2c2b6aab549139ed81c34941a5
nayanApt <nayanjd@aptsoftware.com>
2020-03-25 14:49:03 +0530

    Calling findViewById only once

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,10 +6,13 @@ import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener{
             rollDice()
         }
@@ -17,7 +20,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice()  {
         val randomInt = (1..6).random()
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
